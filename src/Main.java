1b28630beea49c84e8270a0fb30933dbec38fd32
StasonS <stan.stryzh@gmail.com>
2017-06-28 12:45:08 +0300

    Написал программу и запушил на ГитХаб

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Лето быдет шикарным!! \n" +
+                "Будет много солнца, отдыха, приятных моментов\n" +
+                "Но будет и много работы и учёбы....");
+    }
+}
